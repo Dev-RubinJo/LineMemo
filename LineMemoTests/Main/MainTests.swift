20d7492e9ec05ad8900976269094adcf4972906a
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-11 21:06:22 +0900

    Main Domain 테스트 코드 작성

---
+++ LineMemoTests/Main/MainTests.swift
@@ -0,0 +1,35 @@
+//
+//  MainTests.swift
+//  LineMemoTests
+//
+//  Created by YooBin Jo on 2020/02/11.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import XCTest
+@testable import LineMemo
+
+class MainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testMakeMainVC() {
+        let vc = MainVC.viewRouter.makeSplashVC()
+        
+        XCTAssertNotNil(vc)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

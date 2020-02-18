cdbaab256a43aba980d628e7e66968d72c5ca3e4
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-18 12:21:08 +0900

    튜토리얼 메모 안내 문구 수정

--- LineMemo/Memo/Data/DataStruct/Memo.swift
+++ LineMemo/Memo/Data/DataStruct/Memo.swift
@@ -52,7 +52,7 @@ class Memo: Object {
     // MARK: Default Memo
     private static func createDefaultMemo(in realm: Realm) -> Memo {
         let id = UserDefaults.standard.integer(forKey: "memoIdNumber")
-        let memo = Memo(id: id, title: "Tutorial", content: "튜토리얼입니다. 해당 어플리케이션은 메모장 어플리케이션입니다. 위 + 모양의 사각형을 눌러서 이미지를 첨부 할 수 있습니다. 또한 첨부된 이미지는 해당 이미지를 꾹 누르고 계시면 삭제를 할 수가 있고, 한번 누르면 이미지를 크게 볼 수 있습니다.", imageList: [])
+        let memo = Memo(id: id, title: "Tutorial", content: "튜토리얼입니다. 해당 어플리케이션은 메모장 어플리케이션입니다. 위 + 모양의 사각형을 눌러서 이미지를 첨부 할 수 있습니다. 또한 첨부된 이미지는 해당 이미지를 꾹 누르고 계시면 삭제를 할 수가 있고, 한번 누르면 이미지를 크게 볼 수 있습니다. 이미지를 크게 볼때는 첫 이미지부터 볼 수 있습니다.", imageList: [])
         UserDefaults.standard.set(id + 1, forKey: "memoIdNumber")
         try! realm.write {
             realm.add(memo)

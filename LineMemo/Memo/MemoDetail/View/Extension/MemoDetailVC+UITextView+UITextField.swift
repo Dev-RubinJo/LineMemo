994f652d1c1dab9684a6ecc573f5bbb462c59140
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-19 12:11:57 +0900

    불필요 Delegate 제거 및 textView delegate에서 작동할 로직 수정

--- LineMemo/Memo/MemoDetail/View/Extension/MemoDetailVC+UITextView+UITextField.swift
+++ LineMemo/Memo/MemoDetail/View/Extension/MemoDetailVC+UITextView+UITextField.swift
@@ -1,5 +1,5 @@
 //
-//  MemoDetailVC+UITextView+UITextField.swift
+//  MemoDetailVC+UITextView.swift
 //  LineMemo
 //
 //  Created by YooBin Jo on 2020/02/15.
@@ -11,16 +11,18 @@ import UIKit
 extension MemoDetailVC: UITextViewDelegate {
     
     func setMemoDetailTextView(_ textView: UITextView) {
-        if textView.text == "내용을 입력해주세요" {
-            textView.text = ""
-            if #available(iOS 11.0, *) {
-                textView.textColor = UIColor(named: "TextColor")
-            } else {
-                textView.textColor = .black
+        if self.memoData == nil {
+            if textView.text == "내용을 입력해주세요" {
+                textView.text = ""
+                if #available(iOS 11.0, *) {
+                    textView.textColor = UIColor(named: "TextColor")
+                } else {
+                    textView.textColor = .black
+                }
+            } else if textView.text == "" {
+                textView.text = "내용을 입력해주세요"
+                textView.textColor = self.textViewPlaceholderColor
             }
-        } else if textView.text == "" {
-            textView.text = "내용을 입력해주세요"
-            textView.textColor = self.textViewPlaceholderColor
         }
     }
     
@@ -33,7 +35,4 @@ extension MemoDetailVC: UITextViewDelegate {
     func textViewDidEndEditing(_ textView: UITextView) {
         self.setMemoDetailTextView(textView)
     }
-}
-extension MemoDetailVC: UITextFieldDelegate {
-    
 }
